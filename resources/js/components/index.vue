fe5e17b6c898a15e07948b9efb89079b777575fa
Peter verhaar <peter_Verhaar@live.nl>
2023-06-23 21:46:00 +0200

    styling issue

--- resources/js/components/index.vue
+++ resources/js/components/index.vue
@@ -8,13 +8,13 @@
 			<div class="row">
 				<!-- ----------------- Dienst ----------------- -->
 				<div v-for="dienst in diensten" class="col-12 col-md-12 col-lg-12 col-xl-12 mb-4">
-					<div id="dienst" class="dienst">
+					<div id="homeDienst" class="homeDienst">
 						<div class="d-flex">
-							<span class="dienstNaam p-2 fs-4 bg-white w-100 fw-bold">{{ dienst.naam }}</span>
-							<span class="dienstPrijs p-2 fs-3 bg-dark-purple fw-bold">€{{ dienst.prijs.includes('.00') ? dienst.prijs : dienst.prijs+'.00' }}</span>
+							<span class="homeDienstNaam p-2 fs-4 bg-white w-100 fw-bold">{{ dienst.naam }}</span>
+							<span class="homeDienstPrijs p-2 fs-3 bg-dark-purple fw-bold">€{{ dienst.prijs.includes('.00') ? dienst.prijs : dienst.prijs+'.00' }}</span>
 						</div>
 						<div class="d-flex" v-if="dienst.omschrijving.length > 0">
-							<span class="dienstOmschrijving bg-light-purple p-2 w-100">{{ dienst.omschrijving }}</span>
+							<span class="homeDienstOmschrijving bg-light-purple p-2 w-100">{{ dienst.omschrijving }}</span>
 						</div>
 					</div>
 				</div>
